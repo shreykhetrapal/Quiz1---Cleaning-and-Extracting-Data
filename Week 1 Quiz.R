3dd2000e34f5f149e5b805d1287bf60d6e9eb9b9
Shrey Khetrapal <shrey.khetrapal@gmail.com>
2020-06-17 10:53:52 +0530

    Added question 1 and 5

--- Week 1 Quiz.R
+++ Week 1 Quiz.R
@@ -1,6 +1,24 @@
 #  Quiz 1 Cleaning and Extracting data 
 
-#  Question 4 - Read the XML data on Baltimore restaurants from here:
+# ---- Question 1 ----
+# The American Community Survey distributes downloadable data about United States communities. Download the 2006 microdata survey about housing for the state of Idaho using download.file() from here:
+
+# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv
+
+# and load the data into R. The code book, describing the variable names is here:
+  
+# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf
+
+# How many properties are worth $1,000,000 or more?
+
+download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv','/Users/Shrey/Desktop/Data Science JHU/Quiz1---Cleaning-and-Extracting-Data/housing.csv')
+
+data<-fread('housing.csv')
+
+data[, .N,VAL==24] # To calculate frequency of variable VAL where value is 24
+
+
+#  ----- Question 4 - Read the XML data on Baltimore restaurants from here: -----
   
 #  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml
 
@@ -27,7 +45,7 @@ xmlSApply(rootNode[[1]],xmlValue) # Similar to SApply function but for XML. Prin
 names(rootNode[[1]][[1]]) #OUTPUT : Elements that we need i.e. name, zipode, neighborhood, councildistrict ... etc.
 
 
-# ---- USING XPATH to access elements ---- 
+# USING XPATH to access elements 
 
 xpathSApply(rootNode[[1]][[1]],'//name',xmlValue) #This prints out names of all the rows in the xml file 
 
@@ -39,3 +57,21 @@ table(data==21231) # Number of true values is the required answer.
 
 
 
+
+
+# ----- Question 5 -----
+# The American Community Survey distributes downloadable data about United States communities. 
+# Download the 2006 microdata survey about housing for the state of Idaho using download.file() from here:
+
+# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv
+
+fileURL <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv'
+
+download.file(fileURL,'/Users/Shrey/Desktop/Data Science JHU/Quiz1---Cleaning-and-Extracting-Data/dataFile.csv')
+
+
+library(data.table)
+
+DT <- fread('dataFile.csv')
+
+# Now use system.time(<expression in choices>) to calculate the fastest time
